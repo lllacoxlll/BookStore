0718d6d4efd8c668edc30f9a1559162668a16a6f
lllacoxlll <lllacoxlll@gmail.com>
2023-08-22 18:00:56 -0500

    Added Product class and related View Model

---
+++ BookStore.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BookStore.Models;
+
+namespace BookStore.DataAccess.Repository.IRepository
+{
+	public interface IProductRepository : IRepository<Product>
+	{
+		void Update(Product obj);
+	}
+}
+
--- BookStore.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BookStore.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -5,6 +5,7 @@ namespace BookStore.DataAccess.Repository.IRepository
 	public interface IUnitOfWork
 	{
 		ICategoryRepository Category { get; }
+		IProductRepository Product { get; }
 
 		void Save();
 	}
